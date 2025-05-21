067548640871c37f76982c9ddcd9ef6708cea37e
Jhonatas Portela <jhonatasportela1997@gmail.com>
2025-05-20 21:35:37 -0300

    Botao iniciar e pausar temporizador

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const html = document.querySelector("html");
 const focoBtn = document.querySelector(".app__card-button--foco");
 const curtoBtn = document.querySelector(".app__card-button--curto");
 const longoBtn = document.querySelector(".app__card-button--longo");
+const startPauseBtn = document.querySelector("#start-pause");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
 const musicaFocoInput = document.querySelector("#alternar-musica");
 const musicaFoco = new Audio("/sons/luna-rise-part-one.mp3");
+const somDePause = new Audio("/sons/pause.mp3");
+const somDePlay = new Audio("/sons/play.wav");
+const somTemporizadorZerado = new Audio("/sons/beep.mp3");
 musicaFoco.loop = true;
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
 
 focoBtn.addEventListener("click", () => {
   alterarContexto("foco");
@@ -62,3 +69,31 @@ musicaFocoInput.addEventListener("change", () => {
     musicaFoco.pause();
   }
 });
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    zerar();
+    somTemporizadorZerado.play();
+    alert("Tempo esgotado!");
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log(tempoDecorridoEmSegundos);
+};
+
+startPauseBtn.addEventListener("click", iniciarOuPausar);
+
+function iniciarOuPausar() {
+  if (intervaloId) {
+    zerar();
+    somDePause.play();
+    return;
+  }
+  intervaloId = setInterval(contagemRegressiva, 1000);
+  somDePlay.play();
+}
+
+function zerar() {
+  clearInterval(intervaloId);
+  intervaloId = null;
+}
